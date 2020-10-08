f4edd6bfe5a0cb1b24c258013e225cb02c3513c4
PabloLomeli <51377489+PabloLomeli@users.noreply.github.com>
2020-10-08 13:59:20 -0600

    cleaned up files and adding analysis
    I cleaned up comments in code, fixed valgrind issues with unused struct variables and added analysis pdfs.

--- README.md
+++ README.md
@@ -1,113 +1,25 @@
 # Overview
 
-In this project, you will implement a multithreaded version of the
-mergesort algorithm using the pthread library.  We have provided you
-with a single-threaded version of mergesort that you will be
-converting to use threads. Makefiles have been provided that you can
-use directly.
+Pablo Lomeli CS453 Fall 2020
 
-
-## Learning Objectives
-
-- Take a singled threaded algorithm and safely convert it to threaded
-  to see a performance gain.
-- To gain more experience writing concurrent code.
-- Explore the pthread library
-
-
-## Book references
-
-Read these chapters carefully in order to prepare yourself for this project.
-
-- [Intro to Threads](http://pages.cs.wisc.edu/~remzi/OSTEP/threads-intro.pdf)
-- [Threads API](http://pages.cs.wisc.edu/~remzi/OSTEP/threads-api.pdf)
+In this program a set array size will be sorted by single or multiple threads. Timing will be provided to compare the performance difference between single and multiple threads.
 
 ## Make Concurrent (ABET outcome 1)
 
-Convert the serial mergesort code to use multiple threads using the
-pthread library. Your program should limit the number of threads it
-uses via a command line argument (for example, this could be the total
-number of threads or number of levels before cutting off new thread
-generation). You must get a speedup of at least 2 with 4 or more cores
-to get full credit on this project.  Use n = 100,000,000 elements for
-your testing.
+I have designed my program to be based on how many levels are inputted to decide how many threads to use when sorting. 
 
-NOTE: Just because you see a speedup of 2 does not guarantee full
-credit. You can easily hardcode the number of threads and hardcode the
-branches to get a speedup of 2. Your code MUST work with a variable
-number of threads!  Simply getting a speedup of 2 does not indicate
-that you did the assignment correctly.
+To use the program use this usage guide: 
+mytests <input size> [<seed>] [<number of lvls>] [<enable helper printf()>] [<enable efficiency data mode>]
 
-You will need to update mytests.c to accept a number of threads a
-command line argument. The updated version of mytests should include
-timing results for both the serial and parallel versions of merge
-sort.
+The arguments after "input size" are optional and have default values. For instance, by default
+the helper console print methods and dataMode are disabled.
 
 ## Implementation Analysis (ABET outcome 6)
 
 Create a file named speedup.pdf (in the root directory) that analyses
 the following two aspects of your mergesort solution.
 
-**Efficiency** - Analyse the thread efficiency by examining how the
-threaded implementation’s performance compares to the single threaded
-implementation. Include a chart showing the speedups obtained with
-varying number of threads (from 1 to 8). The speedups should relative
-to the serial mergesort. Please also note the number of cores on the
-system that you are testing.  All tests cases should be run with 100
-million elements. Please discuss whether the speedup increases
-linearly as the number of threads are increased.
+**Efficiency** - For efficiency testing, I used levels 1, 2, 3, 4, and 5 to sort 100 million elements and compared how much faster it was than a single thread. More information is provided by "data analysis efficiency.pdf"
 
-**Effectiveness** - Analyse the effectiveness of your threaded
-implementation by comparing sorting time of your threaded solution to
-the single threaded solution. Run both the serial mergesort and
-threaded mergesort with elements in the range from 1,000,000 to
-100,000,000, incrementing by 1M each time.  The threaded version of
-merge sort should be run with 5 threads for all test cases.  Plot the
-timing results in a line chart (google sheets or excel) and compare
-the results. Please discuss whether the threaded version always
-performs better than the serial version.
-
-HINT: You can make the chart using any tool want (excel, google docs,
-etc.) as long as you can create a pdf as your final
-deliverable. Look in the folder example for what your chart should
-look like.
-
-## Hints
-
-- Do not modify the given serial_mergesort function. Instead create a
-  new parallel_mergesort function that will call serial_mergesort as a
-  base case.
-- You can stop the recursion using the number of levels in the sorting
-  tree or by number of threads. It is simpler to stop it by the number
-  of levels.
-- It may be useful to add an option to mytests.c to output timing
-  results speedup calculation in a CSV format.  This will make it
-  significantly easier to generate the required data for the
-  implementation analysis report.
-
-
-## Grading Rubric
-
-All grading will be executed on onyx.boisestate.edu Submissions
-that fail to compile will not being graded.
-
-- [30 pts] Make concurrent (ABET outcome 1)
-  - [10 pts] You got a speedup of at least 2 with 4 or more cores 
-  - [10 pts] mytests.c accepts a number of threads as a command line argument
-  - [10 pts] Correctly creates new threads
-- [30 pts] Implementation analysis (speedup.pdf ABET outcome 6)
-  - Score will reflect the quality of the analysis.
-- [10 pts] Code quality for **lab.c**
-  - [4 pts] Code is formatted correctly and follows a consistent style
-  - [2 pts] Code is commented when necessary
-  - [4 pts] There is more than just the main function in **mytests.c**
-- [20 pts] Compiler warnings **ALL files**
-  - Each compiler warning will result in a 3 point deduction.
-  - You are not allowed to suppress warnings
-  - You must build with the flags ```-Wall -Wextra -Wpointer-arith -Wstrict-prototypes -std=gnu89```
-- [10 pts] Valgrind reports no memory leaks or read/write errors
-  - As reported by **runval.sh**
-  - This is a **PASS/FAIL** score. 1 read/write error or leaking 1
-    byte will result in a zero for this section. There is no partial
-    credit for this section.
+**Effectiveness** - For effectiveness testing, I used level 3, so only 4 threads, and did a 100 single threaded sorts and 100 threaded sorts. More information is provided by "data analysis effectiveness.pdf". Data used to create the pdf is "data.txt".
 
--- mytests.c
+++ mytests.c
@@ -1,3 +1,10 @@
+/**
+ * Name: Pablo Lomeli
+ * Email: pablolomeli@u.boisestate.edu
+ * Github Username: pablolomeli
+ * Added parallel thread functionality to sort a given number of elements.
+ */
+
 #include <pthread.h> // thread functionality library
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,13 +23,15 @@ void merge(int A[], int p, int q, int r);
 void insertion_sort(int A[], int p, int r);
 double getMilliSeconds(void);
 
-// my function prototypes
+// my function prototypes and variables
 void* parrallel_mergesort(void * arg);
 void printinfo(int array[], int n);
 void dataModeMain(void);
 
 int print;
 int dataMode;
+int lvl;
+int seed;
 
 int currentLevel = 0;
 int totalLevels = 0;
@@ -33,7 +42,6 @@ int arrayLength;
 
 struct threadData{
 int p;
-int q;
 int r;
 };
 
@@ -61,33 +69,48 @@ int main(int argc, char **argv) {
 			exit(1);
 	}
 
+  // amount of elements to be used
 	int n = atoi(argv[1]);
-	int seed = 1;
+ 
+  // determine seed to be used to generate random array
 	if (argc >= 3)
 		seed = atoi(argv[2]);
-  
-  int lvl = 1; // default threads
+  else
+    seed = 1;
+   
+  // thread level
   if (argc >= 4)
     lvl = atoi(argv[3]);
+  else
+    lvl = 1;
   
-  if (argc >= 5)
+  // determine if helper print methods run
+  if (argc >= 5 && n < 11)
     print = atoi(argv[4]);
+  else
+    print = 0;
     
+  // determine if DataMode is enabled. DataMode is a loop that runs 100 times that increments from 1 million to 100 million by a million.
   if(argc == 6)
     dataMode = atoi(argv[5]);
+  else
+    dataMode = 0;
   
+  // error out if theres more than 6 arguments
   if(argc > 6){ // if too many arguments used
 			fprintf(stderr, "Usage: mytests <input size> [<seed>] [<number of lvls>] [<enable helper printf()>] [<enable efficiency data mode>] \n");
 			exit(1);
 	}   
   
-  printf("\ninputSize: %d, seed: %d, lvl: %d, printMode: %d, dataMode: %d \n",n,seed,lvl,print,dataMode);
+  if(print)
+    printf("\ninputSize: %d, seed: %d, lvl: %d, printMode: %d, dataMode: %d \n",n,seed,lvl,print,dataMode);
   
   if(dataMode){
     dataModeMain();
     exit (EXIT_SUCCESS);
   }
   
+  //GIVEN CODE STARTS HERE
 	int *A = (int *) malloc(sizeof(int) * (n+1)); // n+1 since we are using A[1]..A[n]
  
 	// generate random input
@@ -112,44 +135,53 @@ int main(int argc, char **argv) {
   if(print)
 	  printinfo(A, n);
   
-  printf("Default has completed.\n");
+  if(print)
+    printf("Default has completed.\n");
   free(A);
+  // GIVEN CODE ENDS HERE
+  
   
   // START OF MY CODE: generate random input to be used by threads.
+  // reset timing values
   start_time = 0;
   sorting_time = 0;
-  long i = 1;
-  masterArray = (int *)malloc( (sizeof(int) * (n+1)) );
   
-  if(lvl > 1)
-    for(i=0; i < lvl-1; i++)
-      threadsUsed *= 2;
-	// generate random input
+  // create new array for threaded sorting
+  masterArray = (int *)malloc( (sizeof(int) * (n+1)) );
   generate_random_array(masterArray,n,seed);
- 
-  //determine how many levels of threads
-  //printf("\nThreads to be used: %d\n", threadsUsed);
-
-  // thread critical values
+  
+  // determine how many threads will be used depending on the level.
+  if(print)
+  {
+    long i = 1;
+    if(lvl > 1)
+      for(i=0; i < lvl-1; i++)
+        threadsUsed *= 2;
+        
+    printf("\nThreads to be used: %d\n", threadsUsed);
+  }
+  
+  // thread data
   totalLevels = lvl;
   arrayLength = n;
   currentLevel= 1;
   
-  pthread_t tid[1]; // init threads, for starters we only have 1 thread.
-  int error;
+  pthread_t tid[1]; // root thread to hop into parallel_mergesort()
   
+  // thread data to be passed
   struct threadData *d = (struct threadData*)malloc(sizeof(d));
   d->p = 1;
   d->r = arrayLength;
-  d->q = d->r + (d->p - d->r)/2;
   
+  int error;
   start_time = getMilliSeconds();
   
+  // start of thread
   error = pthread_create(&(tid[0]), NULL, &parrallel_mergesort, (void*)d);
   if(error != 0)
     printf("\n Thread can't be created : [%s]", strerror(error));
   
-  pthread_join(tid[0], NULL); // ends thread.
+  pthread_join(tid[0], NULL); // wait for root thread
   
   sorting_time = getMilliSeconds() - start_time;
 	
@@ -158,12 +190,14 @@ int main(int argc, char **argv) {
   
 	// print results if correctly sorted otherwise cry foul and exit
 	if (check_if_sorted(masterArray, n)) {
-		printf("\nSorting %d elements took %4.2lf seconds.\n", n,  sorting_time/1000.0);
+		printf("\nThreaded Sorting %d elements took %4.2lf seconds.\n", n,  sorting_time/1000.0);
 	} 
   else {
 		printf("%s: sorting failed!!!!\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
+ 
+  // Free alloc'd memory
 	free(masterArray);
   free(d);
 	exit(EXIT_SUCCESS);
@@ -172,47 +206,57 @@ int main(int argc, char **argv) {
 // MY METHODS
 
 void* parrallel_mergesort(void * arg)
-{  
+{
+  // grab critical data  
   struct threadData *tD = arg;
   int left = tD->p;
   int right = tD->r;
   int middle = right + (left - right)/2 - 1;
   
+  // base case, if level is 1, then do just do serial_mergesort once within the root thread.
   if(totalLevels == 1){
     serial_mergesort(masterArray, 1, arrayLength);
     return NULL;
   }
+  // if level > 1, then 2 or more threads will begin doing serial merge sort
   else if(currentLevel < totalLevels){
     currentLevel++;
-    //printf("\nLeft:%d\n",left);
-    //printf("\nRight:%d\n",right);
-    //printf("\nMiddle:%d\n",middle);
     
+    if(print){
+      printf("\nLeft:%d\n",left);
+      printf("\nRight:%d\n",right);
+      printf("\nMiddle:%d\n",middle);
+    }
+    
+    // init two threads to be used
     pthread_t tid[2];
     
+    // thread 0 data setting
     struct threadData *newtD1 = (struct threadData*)malloc(sizeof(newtD1));
     newtD1->p = left;
     newtD1->r = middle;
     
-    //startThread1
+    //start Thread 0
     int error;
     error = pthread_create(&(tid[0]), NULL, &parrallel_mergesort, (void*)newtD1);
     if(error != 0)
       printf("\n Thread can't be created : [%s]", strerror(error));
     
+    // thread 1 data setting
     struct threadData *newtD2 = (struct threadData*)malloc(sizeof(newtD2));
     newtD2->p = middle+1;
     newtD2->r = right;
     
-    //startThread2
+    //start Thread 1
     error = pthread_create(&(tid[1]), NULL, &parrallel_mergesort, (void*)newtD2);
     if(error != 0)
       printf("\n Thread can't be created : [%s]", strerror(error));
       
-    // both threads end.
+    // wait for both threads
     pthread_join(tid[0], NULL);
     pthread_join(tid[1], NULL);
     
+    // finally merge and free alloc'd memory
     merge(masterArray, left, middle, right);
     free(newtD1);
     free(newtD2);
@@ -236,21 +280,23 @@ void printinfo(int A[], int n)
 }
 
 void dataModeMain(void){
-  printf("success\n");
-  int n = 1000000;
-  int seed = 1;
+  printf("DataMode Enabled.\n");
+  int n = 1000000; // 1 Mill initial element size
+  int seed = 1; // seed 1
   int lvl = 3; // 4 threads
   
+  // data.txt creation and set it to write mode
   FILE *fp;
-
   fp = fopen("data.txt", "w+");
   fprintf(fp, "Beginning of text file\n");
   
+  // loop 100 times incrementing element size by 1 million
   int i;
   for(i=0;i<100;i++)
   {
-    int *A = (int *) malloc(sizeof(int) * (n+1)); // n+1 since we are using A[1]..A[n]
-     
+      //GIVEN CODE STARTS HERE
+  	int *A = (int *) malloc(sizeof(int) * (n+1)); // n+1 since we are using A[1]..A[n]
+   
   	// generate random input
   	generate_random_array(A,n, seed);
     
@@ -264,10 +310,9 @@ void dataModeMain(void){
    
     // print results if correctly sorted otherwise cry foul and exit
   	if (check_if_sorted(A,n)) {
-  		printf("\nSorting %d elements took %4.2lf seconds.\n", n,  sorting_time/1000.0);
-      fprintf(fp, "Serial, %d, %4.2lf", n,  sorting_time/1000.0);
-  	} 
-    else {
+  		printf("Sorting %d elements took %4.2lf seconds.\n", n,  sorting_time/1000.0);
+      fprintf(fp, "Serial, %d, %4.2lf\n", n,  sorting_time/1000.0);
+  	} else {
   		printf("mytests: sorting failed!!!!\n");
   		exit(EXIT_FAILURE);
   	}
@@ -275,44 +320,53 @@ void dataModeMain(void){
     if(print)
   	  printinfo(A, n);
     
-    printf("Default has completed.\n");
+    if(print)
+      printf("Default has completed.\n");
     free(A);
+    // GIVEN CODE ENDS HERE
+    
     
     // START OF MY CODE: generate random input to be used by threads.
+    // reset timing values
     start_time = 0;
     sorting_time = 0;
-    long i = 1;
-    masterArray = (int *)malloc( (sizeof(int) * (n+1)) );
     
-    if(lvl > 1)
-      for(i=0; i < lvl-1; i++)
-        threadsUsed *= 2;
-  	// generate random input
+    // create new array for threaded sorting
+    masterArray = (int *)malloc( (sizeof(int) * (n+1)) );
     generate_random_array(masterArray,n,seed);
-   
-    //determine how many levels of threads
-    //printf("\nThreads to be used: %d\n", threadsUsed);
-  
-    // thread critical values
+    
+    // determine how many threads will be used depending on the level.
+    if(print)
+    {
+      long i = 1;
+      if(lvl > 1)
+        for(i=0; i < lvl-1; i++)
+          threadsUsed *= 2;
+          
+      printf("\nThreads to be used: %d\n", threadsUsed);
+    }
+    
+    // thread data
     totalLevels = lvl;
     arrayLength = n;
     currentLevel= 1;
     
-    pthread_t tid[1]; // init threads, for starters we only have 1 thread.
-    int error;
+    pthread_t tid[1]; // root thread to hop into parallel_mergesort()
     
+    // thread data to be passed
     struct threadData *d = (struct threadData*)malloc(sizeof(d));
     d->p = 1;
     d->r = arrayLength;
-    d->q = d->r + (d->p - d->r)/2;
     
+    int error;
     start_time = getMilliSeconds();
     
+    // start of thread
     error = pthread_create(&(tid[0]), NULL, &parrallel_mergesort, (void*)d);
     if(error != 0)
       printf("\n Thread can't be created : [%s]", strerror(error));
     
-    pthread_join(tid[0], NULL); // ends thread.
+    pthread_join(tid[0], NULL); // wait for root thread
     
     sorting_time = getMilliSeconds() - start_time;
   	
@@ -321,13 +375,15 @@ void dataModeMain(void){
     
   	// print results if correctly sorted otherwise cry foul and exit
   	if (check_if_sorted(masterArray, n)) {
-  		printf("\nSorting %d elements took %4.2lf seconds.\n", n,  sorting_time/1000.0);
-      fprintf(fp, "Threaded, %d, %4.2lf", n,  sorting_time/1000.0);
+  		printf("Threaded Sorting %d elements took %4.2lf seconds.\n", n,  sorting_time/1000.0);
+      fprintf(fp, "Threaded, %d, %4.2lf\n", n,  sorting_time/1000.0);
   	} 
     else {
   		printf("mytests: sorting failed!!!!\n");
   		exit(EXIT_FAILURE);
   	}
+   
+    // Free alloc'd memory
   	free(masterArray);
     free(d);
     
